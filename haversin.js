f039deb8b1f79f6ec007167b2526875886a30252
Gaurav Gautam <gaurav@goldcast.io>
2023-02-24 16:15:15 +0530

    Abandon buffer approach for now

--- haversin.js
+++ haversin.js
@@ -39,6 +39,101 @@ function generateMultipleCoordinatePairs(numPairs) {
 	return jsonFrag.join(',');
 }
 
+const templates = {};
+
+// This is slower. Maybe profiling this function will tell us why 
+function generateMultipleCoordinatePairs2(numPairs) {
+	if (!templates[numPairs]) {
+		templates[numPairs] = makeTemplate(numPairs);
+	}
+
+	const coords = new Array(numPairs * 4).fill(0);
+	for (let i = 0; i < coords.length; ++i) {
+		coords[i] = Math.random()*200 - 100;
+	}
+
+	const template = templates[numPairs];
+	const BUFFLENGTH = 109;
+	const X1_OFFSET = 7;
+	const Y1_OFFSET = 34;
+	const X2_OFFSET = 61;
+	const Y2_OFFSET = 88;
+
+	let coordIndex = 0;
+	for (let pairIndex = 0; pairIndex < numPairs; ++pairIndex) {
+		const startIndex = BUFFLENGTH * pairIndex + pairIndex;
+
+		let coord = coords[coordIndex++].toString().padStart(19, ' ');
+		let coordCharIndex = 0;
+		for (let charIndex = startIndex + X1_OFFSET; 
+			charIndex < startIndex + X1_OFFSET + 19;
+			++charIndex, ++coordCharIndex)
+		{
+			template[charIndex] = coord.charCodeAt(coordCharIndex);
+		}
+
+		coord = coords[coordIndex++].toString().padStart(19, ' ');
+		coordCharIndex = 0;
+		for (let charIndex = startIndex + Y1_OFFSET; 
+			charIndex < startIndex + Y1_OFFSET + 19;
+			++charIndex, ++coordCharIndex)
+		{
+			template[charIndex] = coord.charCodeAt(coordCharIndex);
+		}
+
+		coord = coords[coordIndex++].toString().padStart(19, ' ');
+		coordCharIndex = 0;
+		for (let charIndex = startIndex + X2_OFFSET; 
+			charIndex < startIndex + X2_OFFSET + 19;
+			++charIndex, ++coordCharIndex)
+		{
+			template[charIndex] = coord.charCodeAt(coordCharIndex);
+		}
+
+		coord = coords[coordIndex++].toString().padStart(19, ' ');
+		coordCharIndex = 0;
+		for (let charIndex = startIndex + Y2_OFFSET; 
+			charIndex < startIndex + Y2_OFFSET + 19;
+			++charIndex, ++coordCharIndex)
+		{
+			template[charIndex] = coord.charCodeAt(coordCharIndex);
+		}
+
+	}
+	return templates[numPairs];
+}
+
+function makeTemplate(numPairs) {
+	const template = `{"x1": ___________________, "y1": ___________________, "x2": ___________________, "y2": ___________________ }`;
+	const buf = new Uint8Array(template.length);
+	for (let i = 0; i < template.length; i++) {
+		buf[i] = template.charCodeAt(i);
+	}
+
+	const finalBuf = new Uint8Array(buf.length * numPairs + numPairs);
+	let bufIndex = 0;
+	for (let pairIndex = 0; pairIndex < numPairs; ++pairIndex) {
+		let charIndex = 0;
+		while (charIndex < buf.length) {
+			finalBuf[bufIndex++] = buf[charIndex++];
+		}
+
+		if (bufIndex < numPairs * buf.length - 2) {
+			finalBuf[bufIndex++] = ",".charCodeAt(0);
+		}
+	}
+
+	return finalBuf;
+}
+
+function printBuffer(buf) {
+	let text = '';
+	for (let i = 0; i < buf.length; ++i) {
+		text += String.fromCharCode(buf[i]);
+	}
+	console.log(text);
+}
+
 async function createAFile() {
 	const datapoints = document.querySelector("#generation input");
 	const progress = document.querySelector("#generation #progress");
@@ -62,23 +157,29 @@ async function createAFile() {
 
 	const start_time = performance.now();
 
-	const pairChunkSize = 512;
+	const pairChunkSize = 1024;
 	for (let i = 0; i < (numPoints/pairChunkSize) - 1; ++i) {
-		const pair = generateMultipleCoordinatePairs(pairChunkSize) + ","
+		const pair = generateMultipleCoordinatePairs(pairChunkSize) + ','
 		await writableStream.write(pair);
-		if (showingProgress && ((i % 1024) == 0)) {
+		if (showingProgress && ((i % (512)) == 0)) {
 			progresslevel.style.width = (fullWidth * ((i*pairChunkSize) / numPoints)) + 'px';
 		}
 	}
 
-	const end_time = performance.now();
-
-	await writableStream.write(generateMultipleCoordinatePairs(4));
+	await writableStream.write(generateMultipleCoordinatePairs(pairChunkSize));
 	await writableStream.write('] }');
+	const mid_time = performance.now();
+	progresslevel.style.background = 'green';
 	await writableStream.close();
+
+	const end_time = performance.now();
 	progress.style.display = "none";
 
-	resultdiv.innerText = (end_time - start_time) + ' ms, ' + ((end_time - start_time)/1000) + ' s';
+	resultdiv.innerText = `
+		Total time: ${end_time - start_time} ms
+		Time for writing to stream: ${mid_time - start_time} ms
+		Time for closing stream: ${end_time - mid_time} ms
+	`;
 	// alert("File creation complete! You can click on Read file to view the result");
 }
 
