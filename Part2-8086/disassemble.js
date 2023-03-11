f0c34da1c3403174079229f195a77efcf3bb745a
Gaurav Gautam <gaurav@goldcast.io>
2023-03-11 08:01:22 +0530

    Cleaned out part2

---
+++ Part2-8086/disassemble.js
@@ -0,0 +1,34 @@
+function showBinaryContents(bytes) {
+	const binaryDisplay = document.querySelector("#binarydata");
+	let contents = "";
+	for (let i = 0; i < bytes.length; ++i) {
+		contents += bytes[i].toString(2).padStart(8, '0') + ' ';
+	}
+	binaryDisplay.innerText = contents;
+}
+
+async function getFileBytes() {
+	const [newFileHandle] = await window.showOpenFilePicker();
+	const file = await newFileHandle.getFile();
+	const buffer = await file.arrayBuffer();
+	const bytes = new Uint8Array(buffer);
+	return bytes;
+}
+
+async function showFileBinary() {
+	const bytes = await getFileBytes();
+	showBinaryContents(bytes);
+}
+
+window.onload = function() {
+	let button = document.querySelector("#choosebin");
+	button.addEventListener("click", showFileBinary);
+
+	fetch("test")
+		.then(res => res.blob())
+		.then(res => res.arrayBuffer())
+		.then(res => {
+			const bytes = new Uint8Array(res);
+			showBinaryContents(bytes);
+		});
+}
