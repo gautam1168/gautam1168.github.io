d309100d94b0d800f83ae6650fccad3bc940cc27
Gaurav Gautam <gaurav@goldcast.io>
2023-03-13 19:33:28 +0530

    Start next assignment

---
+++ Part3-8086/disassemble.js
@@ -0,0 +1,108 @@
+function showBinaryContents(bytes) {
+	const binaryDisplay = document.querySelector("#binarydata");
+	let contents = "";
+	for (let i = 0; i < bytes.length; ++i) {
+		contents += bytes[i].toString(2).padStart(8, '0') + ' ';
+	}
+	binaryDisplay.innerText = contents;
+}
+
+async function getFileBytes() {
+	const [newFileHandle] = await window.showOpenFilePicker();
+	const file = await newFileHandle.getFile();
+	const buffer = await file.arrayBuffer();
+	const bytes = new Uint8Array(buffer);
+	return bytes;
+}
+
+async function showFileBinary() {
+	const bytes = await getFileBytes();
+	showBinaryContents(bytes);
+	const asm = decompile(bytes);
+	const disasmDisplay = document.querySelector("#fulltabledecode");
+	disasmDisplay.innerHTML = asm.join('<br/>');
+}
+
+async function loadDecodeTable() {
+	const res = await fetch("decodetable.txt")
+	const rawData = await res.text();
+
+	const lines = rawData.split("\n");
+	window.translationKey = new Array(65536);
+	for (let i = 0; i < 65536; ++i) {
+		const translation = lines[i].split(' ');
+		if (translation[1]) {
+			window.translationKey[i] = translation.slice(1).join(' ');
+		}
+	}
+}
+
+function decompile(bytes) {
+	let byteIndex = 0;
+	let result = [];
+	const EightBitCaster = new Int8Array(1);
+	const SixteenBitCaster = new Int16Array(1);
+	while (byteIndex < bytes.length) {
+		const firstByte = bytes[byteIndex++];
+		let translation = window.translationKey[firstByte];
+		if (translation) {
+			const code = translation.split(";");
+			let interimCode = code[0];
+			const numBytesToReadArray = code[1].split(",").map(it => parseInt(it));
+			for (let numBytesToRead of numBytesToReadArray) {
+				if (numBytesToRead == 1) {
+					const value = bytes[byteIndex++];
+					EightBitCaster[0] = value;
+					interimCode = interimCode.replace("{bytes}", EightBitCaster[0]);
+				} else if (numBytesToRead == 2) {
+					const value = ((bytes[byteIndex + 1] << 8) | bytes[byteIndex]);
+					SixteenBitCaster[0] = value;
+					interimCode = interimCode.replace("{bytes}", SixteenBitCaster[0]);
+					byteIndex += 2;
+				}
+			}
+			result.push(interimCode.replace(/\+\-/g, "-"));
+		} else {
+			const secondByte = bytes[byteIndex++];
+			const bigIndex = (firstByte << 8) | secondByte;
+			if (!window.translationKey[bigIndex]) {
+				throw new Error("Translation not found for " + bigIndex + " , " + bigIndex.toString(2).padStart(16, '0'));
+			}
+			translation = window.translationKey[bigIndex].split(";");
+			let interimTran = translation[0];
+			const numBytesToReadArray = translation[1].split(",").map(it => parseInt(it));
+			for (let numBytesToRead of numBytesToReadArray) {
+				if (numBytesToRead == 1) {
+					const value = bytes[byteIndex++];
+					EightBitCaster[0] = value;
+					interimTran = interimTran.replace("{bytes}", EightBitCaster[0]);
+				} else if (numBytesToRead == 2) {
+					const value = ((bytes[byteIndex + 1] << 8) | bytes[byteIndex]);
+					SixteenBitCaster[0] = value;
+					interimTran = interimTran.replace("{bytes}", SixteenBitCaster[0]);
+					byteIndex += 2;
+				}
+			}
+			result.push(interimTran.replace(/\+\-/g, "-"));
+		}
+	}
+	return result;
+}
+
+window.onload = async function() {
+	let button = document.querySelector("#choosebin");
+	button.addEventListener("click", showFileBinary);
+
+	await loadDecodeTable();	
+
+	fetch("listing_0041_add_sub_cmp_jnz")
+		.then(res => res.blob())
+		.then(res => res.arrayBuffer())
+		.then(res => {
+			const bytes = new Uint8Array(res);
+			showBinaryContents(bytes);
+			const asm = decompile(bytes);
+			const disasmDisplay = document.querySelector("#fulltabledecode");
+			disasmDisplay.innerHTML = asm.join('<br/>');
+		});
+}
---
+++ Part3-8086/generateDecodeTable.js
@@ -0,0 +1,260 @@
+const fs = require("fs");
+
+const data = "something to write";
+
+// regName[w][reg]
+const regName = [
+	["AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH"],
+	["AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI"],
+];
+
+// EffAddress[mod][rm]
+// EffAddress[11][w][rm]
+const EffAddress = [
+	[
+		"[BX+SI]", 
+		"[BX+DI]", 
+		"[BP+SI]", 
+		"[BP+DI]", 
+		"[SI]", 
+		"[DI]", 
+		"[{bytes}]", 
+		"[BX]"
+	],
+	[
+		"[BX+SI+{bytes}]", 
+		"[BX+DI+{bytes}]", 
+		"[BP+SI+{bytes}]", 
+		"[BP+DI+{bytes}]", 
+		"[SI+{bytes}]", 
+		"[DI+{bytes}]", 
+		"[BP+{bytes}]", 
+		"[BX+{bytes}]"
+	],
+	[
+		"[BX+SI+{bytes}]", 
+		"[BX+DI+{bytes}]", 
+		"[BP+SI+{bytes}]", 
+		"[BP+DI+{bytes}]", 
+		"[SI+{bytes}]", 
+		"[DI+{bytes}]", 
+		"[BP+{bytes}]", 
+		"[BX+{bytes}]"
+	],
+	[["AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH"],
+	["AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI"]]
+];
+
+fs.open("decodetable.txt", "w", (err, fd) => {
+	if (err) {
+		console.error("Failed to open file", err);
+		return;
+	}
+
+	for (let OpcodeIndex = 0; OpcodeIndex < 65536; ++OpcodeIndex) {
+		
+		Instruction = OpcodeIndex.toString(2).padStart(16, '0');
+		Instruction += " " + getAssemblyTemplate(OpcodeIndex);
+		Instruction += "\n";
+		
+		fs.writeSync(fd, Instruction); 
+	}
+
+	fs.close(fd);
+});
+
+function registerMemoryToFromRegister(OpName, FirstByte, SecondByte) {
+	const d = (FirstByte & 0b10) >> 1;
+	const w = FirstByte & 0b1;
+
+	const mod = (SecondByte >> 6);
+	const reg = (SecondByte & 0b111000) >> 3;
+	const rm = (SecondByte & 0b111);
+	let result = "";
+	if (d == 0) {
+		if (mod == 3) {
+			result = OpName + 
+				" " + EffAddress[mod][w][rm] + ", " + regName[w][reg];
+		} else {
+			result = OpName + 
+				" " + EffAddress[mod][rm] + ", " + regName[w][reg];
+		}
+	} else {
+		if (mod == 3) {
+			result = OpName + 
+				" " + regName[w][reg] + ", " + EffAddress[mod][w][rm];
+		} else {
+			result = OpName + 
+				" " + regName[w][reg] + ", " + EffAddress[mod][rm];
+		}
+	}
+
+	if (mod == 1) {
+		result += " ;1";
+	} else if (mod == 2) {
+		result += " ;2";
+	} else if (mod == 0 && rm == 0b110) {
+		result += " ;" + (w == 0 ? 1 : 2);
+	} else {
+		result += " ;0";
+	}
+	return result;
+}
+
+function getAssemblyTemplate(OpcodeIndex) {
+	const FirstByte = OpcodeIndex >> 8;
+	const FirstSixBits = FirstByte >> 2;
+	const SecondByte = (OpcodeIndex & 0b11111111);
+	// Register/Memory to/from register
+	// 100010,d,w		mod,reg,r/m 	disp-lo 	disp-hi
+	if (FirstSixBits == 0b100010) {
+		return registerMemoryToFromRegister("MOV", FirstByte, SecondByte);
+	} 
+	// Immediate to register/memory
+	// 1100011,w 	mod,000,r/m 	data  data if w=1
+	else if (FirstByte == 0b11000110) {
+		const w = 0;
+		const mod = (SecondByte >> 6);
+		const reg = (SecondByte & 0b111000) >> 3;
+		const rm = (SecondByte & 0b111);
+		let result;
+		if (reg == 0) {
+			if (mod == 3) {
+				result = "MOV " + 
+					" " + EffAddress[mod][w][rm] + ", {bytes}";
+			} else {
+				result = "MOV " + 
+					" " + EffAddress[mod][rm] + ", {bytes}";
+			}
+
+			if (mod == 1) {
+				result += " ;1,1";
+			} else if (mod == 2) {
+				result += " ;2,1";
+			}  else if (mod == 0 && rm == 0b110) {
+				result += " ;2,1";
+			} else {
+				result += " ;1";
+			}
+
+			return result;
+		}
+	} else if (FirstByte == 0b11000111) {
+		const w = 1;
+		const mod = (SecondByte >> 6);
+		const reg = (SecondByte & 0b111000) >> 3;
+		const rm = (SecondByte & 0b111);
+		if (reg == 0) {
+			let result;
+			if (mod == 3) {
+				result = "MOV " + 
+					" " + EffAddress[mod][w][rm] + ", {bytes}";
+			} else {
+				result = "MOV " + 
+					" " + EffAddress[mod][rm] + ", {bytes}";
+			}
+
+			if (mod == 1) {
+				result += " ;1,2";
+			} else if (mod == 2) {
+				result += " ;2,2";
+			}  else if (mod == 0 && rm == 0b110) {
+				result += " ;2,2";
+			} else {
+				result += " ;2";
+			}
+			return result;
+		}
+	}
+	// Immediate to register
+	// 1011,w,reg 	data		data-1 if w=1
+	else if (FirstByte == 0 && SecondByte == 0b10110000) {
+		const w = 0;
+		const reg = 0b000;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110001) {
+		const w = 0;
+		const reg = 0b001;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110010) {
+		const w = 0;
+		const reg = 0b010;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110011) {
+		const w = 0;
+		const reg = 0b011;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110100) {
+		const w = 0;
+		const reg = 0b100;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110101) {
+		const w = 0;
+		const reg = 0b101;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110110) {
+		const w = 0;
+		const reg = 0b110;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10110111) {
+		const w = 0;
+		const reg = 0b111;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111000) {
+		const w = 1;
+		const reg = 0b000;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111001) {
+		const w = 1;
+		const reg = 0b001;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111010) {
+		const w = 1;
+		const reg = 0b010;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111011) {
+		const w = 1;
+		const reg = 0b011;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111100) {
+		const w = 1;
+		const reg = 0b100;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111101) {
+		const w = 1;
+		const reg = 0b101;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111110) {
+		const w = 1;
+		const reg = 0b110;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	} else if (FirstByte == 0 && SecondByte == 0b10111111) {
+		const w = 1;
+		const reg = 0b111;
+		return "MOV " + regName[w][reg] + ", " + (w == 0 ? "{bytes} ;1" : "{bytes} ;2");
+	}
+	// Memory to accumulator
+	// 1010000,w 	addr-lo		addr-hi
+	else if (FirstByte == 0 && SecondByte == 0b10100000) {
+		return "MOV AL, [{bytes}] ;2";
+	} else if (FirstByte == 0 && SecondByte == 0b10100001) {
+		return "MOV AX, [{bytes}] ;2";
+	}
+	// Accumulator to memory
+	// 1010001,w  addr-lo   addr-hi
+	else if (FirstByte == 0 && SecondByte == 0b10100010) {
+		return "MOV [{bytes}], AL ;2";
+	} else if (FirstByte == 0 && SecondByte == 0b10100011) {
+		return "MOV [{bytes}], AX ;2";
+	}
+	// Register/memory to segment register
+	// 10001110 	mod,0,SR,r/m 	 disp-lo		disp-hi
+	else if (FirstByte == 0b10001110 && ((SecondByte & 0b100000) >> 5) == 0) {
+		return "NOT IMPLEMENTED";
+	} 
+	else if (FirstByte == 0b10001100 && ((SecondByte & 0b100000) >> 5) == 0) {
+		return "NOT IMPLEMENTED";
+	}
+	return "";
+}
+
